16002ed9cacd5b3b6bcb75a1579f2c065c1c7f2a
GoAndrii <soki200561@gmail.com>
2022-06-30 15:36:56 +0300

    d

--- js/menu.js
+++ js/menu.js
@@ -8,7 +8,9 @@
   refs.openMenulBtn.addEventListener("click", toggleMenu);
   refs.closeMenulBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
+    function toggleMenu() {
+      document.body.classList.toggle("menu-open");
     refs.menu.classList.toggle("is-hidden");
-  }
+    }
+    
 })();
